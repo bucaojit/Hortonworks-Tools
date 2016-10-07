970aeef85c8452b8054132325b42f3cfb7124a06
Oliver Bucaojit <obucaojit@HW11038.local>
2016-10-07 14:24:35 -0700

    Updated for newer HBase

--- hbase-tools/src/main/java/tools/App.java
+++ hbase-tools/src/main/java/tools/App.java
@@ -6,14 +6,12 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
 public class App 
 {
@@ -25,7 +23,6 @@ public class App
 	    Configuration conf = HBaseConfiguration.create();
 	    conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"));
 	    
-	    HBaseAdmin hbadmin = null;
 	    Connection connection = null;
 	    Admin admin = null;
 	    TableName tablename = null;
@@ -33,10 +30,9 @@ public class App
 	    try {
 	      connection = ConnectionFactory.createConnection(conf);
 	      admin = connection.getAdmin();
-	      hbadmin = new HBaseAdmin(conf);
-	      if(!hbadmin.isTableAvailable("table_example")) {
-	         tablename = TableName.valueOf("table_example");
-	  
+	      tablename = TableName.valueOf("table_example");
+
+	      if(!admin.isTableAvailable(tablename)) {
 	        HTableDescriptor desc = new HTableDescriptor(tablename);
 	        desc.addFamily(new HColumnDescriptor("cf1"));
 	        admin.createTable(desc);
@@ -53,11 +49,20 @@ public class App
 	    }
 
 	    Put put = new Put(Bytes.toBytes("Value to enter"));
-	    put.add(Bytes.toBytes("cf1"), Bytes.toBytes("newQualifier"), Bytes.toBytes("NewValue"));
+	    put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("newQualifier"), Bytes.toBytes("NewValue"));
 	    try {
 	      table.put(put);    
 	    } catch(Exception e) {
 	      e.printStackTrace();
 	    }
+	    // Insert rows for RowCounter
+	    for(int i = 400000; i < 700000; i++) {
+	    	Put insert = new Put(Bytes.toBytes(i));
+	    	try {
+	    		insert.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("newQualifier"), Bytes.toBytes("NewValue"));
+	    		table.put(insert);
+	    	} catch (Exception e) {}
+	    }
+	    
 	  }
 }
