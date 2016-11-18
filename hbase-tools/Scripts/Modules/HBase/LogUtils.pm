b4f17458ce3f84dd2faf10f8f624497e2c39685c
Oliver Bucaojit <obucaojit@gmail.com>
2016-11-17 17:44:28 -0800

    progress

--- hbase-tools/Scripts/Modules/HBase/LogUtils.pm
+++ hbase-tools/Scripts/Modules/HBase/LogUtils.pm
@@ -5,8 +5,20 @@ use feature qw(say);
 
 use Exporter qw(import);
 use File::Find;
+use GetOpt::Long;
+use English qw' -no_match_vars ';
 
 our @EXPORT_OK = qw(getMasterLogs getRSLogs);
+
+# Remember to utilize on mac: open 'http://www.yahoo.com'
+sub isMac {
+  if("darwin" eq $OSNAME) {
+    return 1;
+  }   
+  else  {
+    return 0;
+  }   
+}  
 
 sub getMasterLogs {
   $directory = shift;
@@ -48,6 +60,48 @@ sub getAllFiles {
   my @files;
   find( sub { push @files, $File::Find::name unless -d; }, $directory);
   return @files;
+}
+
+sub processRSLog {
+  
+}
+
+sub processMasterLog {
+
+}
+
+sub getConfigVals {
+
+}
+
+sub getJvmPause {
+
+}
+
+sub checkJvmPause {
+  # Configurable value default 20000ms
+  # If Error, print out the configvals, namely GC_OPTS (vmInput)
+
+}
+
+sub getErrors {
+
+}
+
+sub getZKTimeouts {
+
+}
+
+sub getAborts {
+
+}
+
+sub getPriorToClosing {
+  # Need to figure out how to obtain this.  
+  #   - Maybe it's own run 
+  #   - Save last 100 lines 
+  #   - If you get 20 closings, then print the last 120 to 20 lines
+  return;
 }
 
 1;
