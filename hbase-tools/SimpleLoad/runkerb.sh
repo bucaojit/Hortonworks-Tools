55ea205f3f5a09a420edf2e66c846dbef0932386
Oliver <obucaojit@gmail.com>
2017-03-29 23:43:56 +0000

    adding load with kerberized cluster

---
+++ hbase-tools/SimpleLoad/runkerb.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp target/hbase-example-4.7.1-HBase-1.1.jar main.java.tools.KerbTest $1 $2
--- hbase-tools/SimpleLoad/src/main/java/tools/KerbTest.java
+++ hbase-tools/SimpleLoad/src/main/java/tools/KerbTest.java
@@ -21,8 +21,27 @@ public class KerbTest
 {
 
 	public static void main(String[] args) {
-		
-	    //HTable ht = null;
+	    if(args.length != 2 || args.length != 4) {
+                System.out.println("Usage: KerbTest <PrincipalName> <KeyTab>");
+                return;
+            }
+            Integer start = 0;
+            Integer end   = 100000;
+            String principal, keytab;
+            if(args.length == 2) {
+                principal = args[0];
+                keytab = args[1];
+            }
+            else if(args.length == 4) {
+                principal = args[0];
+                keytab = args[1];
+                start = Integer.parseInt(args[3]);
+                end   = Integer.parseInt(args[4]);
+            }
+              
+            principal = args[0];
+            keytab = args[1];
+
 	    Table table = null;
 	    Configuration conf = HBaseConfiguration.create();
 	    conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"));
@@ -36,9 +55,8 @@ public class KerbTest
 	    Connection connection = null;
 	    Admin admin = null;
 	    TableName tablename = null;
-	    //HTableDescriptor htd = null;
 	    try {
-	      UserGroupInformation.loginUserFromKeytab("hbase-kerbed_2node@OLIVER.COM", "/etc/security/keytabs/hdfs.headless.keytab");
+	      UserGroupInformation.loginUserFromKeytab(principal, keytab);
 	      // hdfs.headless.keytab
 	      connection = ConnectionFactory.createConnection(conf);
 	      admin = connection.getAdmin();
@@ -48,11 +66,8 @@ public class KerbTest
 	        HTableDescriptor desc = new HTableDescriptor(tablename);
 	        desc.addFamily(new HColumnDescriptor("cf1"));
 	        admin.createTable(desc);
-	        //admin.createTable(desc);
 	        
 	      }
-	      //ht = new HTable(conf, "table_example");
-	      //htd = admin.getTableDescriptor(tablename);
 	      table = connection.getTable(tablename);
 	      
 	      
@@ -68,7 +83,7 @@ public class KerbTest
 	      e.printStackTrace();
 	    }
 	    // Insert rows for RowCounter
-	    for(int i = 50000; i < 50002; i++) {
+	    for(int i = start; i < end; i++) {
 	    	String strVal = "'" + i + "'";
 	    	Put insert = new Put(Bytes.toBytes(strVal));
 	    	try {
@@ -76,22 +91,5 @@ public class KerbTest
 	    		table.put(insert);
 	    	} catch (Exception e) {}
 	    }
-	    /*
-
-	    //Put put = new Put(Bytes.toBytes("Value to enter"));
-	    Scan scan = new Scan();
-	    
-	    scan.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("newQualifier"));
-	    ResultScanner scanner = null;
-	    try {
-	         scanner = table.getScanner(scan);
-	    } catch(Exception e) {
-	      e.printStackTrace();
-	    }
-	    for(Result result : scanner) {
-	    	System.out.println(result.getRow().toString());
-	    }
-	    */
-	    
 	  }
 }
